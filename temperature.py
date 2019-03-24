ff8ab9320e767ecba8dffafadab19f483c2df1ce
Tyler Baker <tyler.baker02@mymail.champlain.edu>
2019-03-24 18:29:04 -0400

    Fixed Everything and added Docstrings

--- temperature.py
+++ temperature.py
@@ -1,5 +1,21 @@
+"""Classes for working with Temperatures.
+
+Author: Tyler Baker
+Class: CSI-260-02
+Assignment: Week 10 Lab
+Due Date: March 25, 2019 11:00 AM
+
+Certification of Authenticity:
+I certify that this is entirely my own work, except where I have given
+fully-documented references to the work of others. I understand the definition
+and consequences of plagiarism and acknowledge that the assessor of this
+assignment may, for the purpose of assessing this assignment:
+- Reproduce this assignment and provide a copy to another member of academic
+- staff; and/or Communicate a copy of this assignment to a plagiarism checking
+- service (which may then retain a copy of this assignment on its database for
+- the purpose of future plagiarism checking)
+"""
 import re
-"""Classes for working with Temperatures."""
 
 
 class TemperatureError(Exception):
@@ -15,7 +31,7 @@ class Temperature:
     or Kelvins.
     """
 
-    def __init__(self, degrees):
+    def __init__(self, degrees=0):
         """Initialize temperature with specified degrees.
 
         Args:
@@ -29,8 +45,8 @@ class Temperature:
                        K, in which case it is interpreted as Kelvins
 
         Raises:
-            TemperatureError: if degrees is not one of the specified
-                                     forms
+            TemperatureError: if degrees is not one of the specified forms
+
         """
         try:
             self.celsius = degrees
@@ -39,18 +55,18 @@ class Temperature:
 
     @property
     def celsius(self):
-        """Celsius value of the temperature"""
+        """Celsius value of the temperature."""
         return self._celsius
 
     @celsius.setter
     def celsius(self, celsius):
-        """Sets the value of celsius"""
+        """Set the value of celsius."""
         if isinstance(celsius, str):
             check = str(re.sub('[^a-zA-Z]', '', celsius)).lower()
             if check != 'c' and check != 'k' and check != 'f' and check != '':
                 raise TemperatureError
             else:
-                celsius = float(re.sub('[a-zA-Z ]', '', celsius))
+                celsius = float(re.sub('[a-zA-Z]', '', celsius))
                 if check == 'k':
                     celsius -= 273.15
                 if check == 'f':
@@ -61,47 +77,47 @@ class Temperature:
 
     @property
     def fahrenheit(self):
-        """Celsius value of the temperature"""
+        """Fahrenheit value of temperature."""
         return (self._celsius * 9/5) + 32
 
     @fahrenheit.setter
-    def fahrenheit(self, celsius):
-        """Sets the value of celsius"""
-        if isinstance(celsius, str):
-            check = str(re.sub('[^a-zA-Z]', '', celsius)).lower()
+    def fahrenheit(self, fahrenheit):
+        """Set the value of celsius."""
+        if isinstance(fahrenheit, str):
+            check = str(re.sub('[^a-zA-Z]', '', fahrenheit)).lower()
             if check != 'c' and check != 'k' and check != 'f' and check != '':
                 raise TemperatureError
             else:
-                celsius = float(re.sub('[a-zA-Z ]', '', celsius))
+                fahrenheit = float(re.sub('[a-zA-Z]', '', fahrenheit))
                 if check == 'k':
-                    celsius -= 273.15
-                if check == 'f':
-                    celsius = (celsius - 32) * 5 / 9
-        elif not isinstance(celsius, (int, float)):
+                    fahrenheit = ((fahrenheit - 273.15) * 9/5) + 32
+                if check == 'c':
+                    fahrenheit = (fahrenheit * 9/5) + 32
+        elif not isinstance(fahrenheit, (int, float)):
             raise TemperatureError
-        self._celsius = celsius
+        self._celsius = (fahrenheit - 32) * 5/9
 
     @property
     def kelvin(self):
-        """Celsius value of the temperature"""
+        """Convert Celsius to kelvin and return it."""
         return self._celsius + 273.15
 
     @kelvin.setter
-    def kelvin(self, celsius):
-        """Sets the value of celsius"""
-        if isinstance(celsius, str):
-            check = str(re.sub('[^a-zA-Z]', '', celsius)).lower()
+    def kelvin(self, kelvin):
+        """Set the value of celsius."""
+        if isinstance(kelvin, str):
+            check = str(re.sub('[^a-zA-Z]', '', kelvin)).lower()
             if check != 'c' and check != 'k' and check != 'f' and check != '':
                 raise TemperatureError
             else:
-                celsius = float(re.sub('[a-zA-Z ]', '', celsius))
-                if check == 'k':
-                    celsius -= 273.15
+                kelvin = float(re.sub('[a-zA-Z]', '', kelvin))
+                if check == 'c':
+                    kelvin += 273.15
                 if check == 'f':
-                    celsius = (celsius - 32) * 5 / 9
-        elif not isinstance(celsius, (int, float)):
+                    kelvin = (kelvin - 32) * 5/9 + 273.15
+        elif not isinstance(kelvin, (int, float)):
             raise TemperatureError
-        self._celsius = celsius
+        self._celsius = kelvin - 273.15
 
     @classmethod
     def average(cls, temperatures):
